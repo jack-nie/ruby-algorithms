9dd50084dfb757dfe53ce99f669d6814de963675
jack-nie <hust.ud.hui@gmail.com>
2015-09-11 23:42:04 +0800

    delete benchmark code from insert sort source code

--- algorithms/insert_sort.rb
+++ algorithms/insert_sort.rb
@@ -32,9 +32,3 @@ module Algorithms
     end
   end
 end
-
-require "benchmark"
-Benchmark.bm do |x|
-  x.report{ 50000.times do Algorithms::InsertionSort.insert_sort_one [3,5,2,7,9,6]; end}
-  x.report{ 50000.times do Algorithms::InsertionSort.insert_sort [3,5,2,7,9,6]; end}
-end
