b4ebc81a08ca47b16f549953ea4f4cdac98ca8e1
jack-nie <hust.ud.hui@gmail.com>
2015-09-19 18:59:49 +0800

    add bubble sort

---
+++ algorithms/bubble_sort.rb
@@ -0,0 +1,43 @@
+module  Algorithms
+  class BubbleSort
+
+    class << self
+
+      def bubble_sort arr
+        length = arr.length
+        loop do
+          swapped = false
+          1.upto(length - 1) do |i|
+            if arr[i - 1] > arr[i] 
+              arr[i - 1], arr[i] = arr[i], arr[i - 1]
+              swapped = true
+            end
+          end
+          break unless swapped
+        end
+        arr
+      end
+
+
+      def bubble_sort_one arr
+        length = arr.length
+        loop do
+          swapped = false
+          1.upto(length - 1) do |i|
+            if arr[i - 1] > arr[i] 
+              arr[i - 1], arr[i] = arr[i], arr[i - 1]
+              swapped = true
+            end
+          end
+          length -= 1
+          break unless swapped
+        end
+        arr
+      end
+
+    end
+  end
+end
+
+Algorithms::BubbleSort.bubble_sort([3,2,5,6,8,7]).map(&method(:p))
+
